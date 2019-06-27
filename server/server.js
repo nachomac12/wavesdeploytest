628965bcc9b1d8bed192ab4ef64d52f3f7af8849
nachomac12 <maciasignacio0@gmail.com>
2019-06-27 11:49:37 -0300

    asjkas

--- server/server.js
+++ server/server.js
@@ -10,11 +10,13 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -415,6 +417,13 @@ app.post('/api/users/update_profile',auth,(req,res)=>{
         }
     );
 })
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/*', (req, res) => {
+        res.sendfile(path.resolve(__dirname, '../client', 'build', 'index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 3002;
